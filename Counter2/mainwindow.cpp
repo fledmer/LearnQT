4c5b9e9ebe35ce1f9c4ae0a5e19e66036f59db60
fledmer <62475445+fledmer@users.noreply.github.com>
2021-06-12 23:59:12 +0300

    Add Counter2
    Using menu

---
+++ Counter2/mainwindow.cpp
@@ -0,0 +1,53 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+#include <QFrame>
+#include <QGridLayout>
+#include <QPushButton>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+    , couter(0)
+{
+    QAction *plusAction = new QAction("Plus",this);
+    QAction *minusAction = new QAction("Minus",this);
+    QAction *exitAction = new QAction("Exit",this);
+    LCDcouter = new QLCDNumber(this);
+
+    QMenu *actionMenu;
+    actionMenu = menuBar()->addMenu("Action");
+    actionMenu->addAction(plusAction);
+    actionMenu->addAction(minusAction);
+    actionMenu->addAction(exitAction);
+
+    QGridLayout *buttonGrid = new QGridLayout(this);
+    buttonGrid->addWidget(LCDcouter,0,0);
+
+
+    connect(plusAction,&QAction::triggered,this,&MainWindow::plusSlot);
+    connect(minusAction,&QAction::triggered,this,&MainWindow::minusSlot);
+    connect(exitAction,&QAction::triggered,this,&QApplication::quit);
+
+    QWidget* mainWidget = new QWidget;
+    mainWidget->setLayout(buttonGrid);
+    setCentralWidget(mainWidget);
+}
+
+void MainWindow::plusSlot()
+{
+  couter++;
+  LCDcouter->display(couter);
+}
+
+void MainWindow::minusSlot()
+{
+  couter--;
+  LCDcouter->display(couter);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
