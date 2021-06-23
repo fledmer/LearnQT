6c25743a65c088cfdc1e9f32f1ddd204678cc999
fledmer <thesteelzo11@gmail.com>
2021-06-23 17:14:01 +0300

    update Sladers

--- Сладеры/form.cpp
+++ Сладеры/form.cpp
@@ -7,9 +7,23 @@
 #include "QStyle"
 #include "QDial"
 #include "QLabel"
+#include "QGroupBox"
+#include "QLineEdit"
+
 
 Form::Form(QWidget *parent) : QWidget(parent)
 {
+    QHBoxLayout *mainLayout = new QHBoxLayout;
+    mainLayout->setSpacing(1);
+    setLayout(mainLayout);
+    QFont baseLabelFont("Times New Roman",15);
+
+    QSlider *slider;
+    {
+    QGroupBox *firstGroupBox = new QGroupBox("Штуки-дрюки",this);
+    mainLayout->addWidget(firstGroupBox);
+    setLayout(mainLayout);
+
     QLCDNumber *lcdNumber = new QLCDNumber();
     lcdNumber->setSegmentStyle(QLCDNumber::Flat);
     //lcdNumber->setPalette(Qt::darkGreen);
@@ -17,15 +31,28 @@ Form::Form(QWidget *parent) : QWidget(parent)
     lcdNumber->setStyleSheet("color: #009a63");
     lcdNumber->setMode(QLCDNumber::Hex);
 
-    QFont baseLabelFont("Times New Roman",15);
 
     QLabel *sliderLabel = new QLabel("&Слайдер:");
     sliderLabel->setFont(baseLabelFont);
-    QSlider *slider = new QSlider(Qt::Horizontal);
+    slider = new QSlider(Qt::Horizontal);
     sliderLabel->setBuddy(slider);
     slider->setRange(0,100);
     slider->setTickPosition(QSlider::TicksBothSides);
     slider->setTickInterval(10);
+    slider->setStyleSheet(
+                "QSlider::groove:horizontal {\
+                    border: 1px solid #999999;\
+                    height: 8px;\
+                    background: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #007c00, stop:1 #00e400);\
+                    margin: 2px 0;\
+                }\
+                QSlider::handle:horizontal {\
+                    background: qlineargradient(x1:0, y1:0, x2:1, y2:1, stop:0 #00e400, stop:1 #007c00);\
+                    border: 1px solid #5c5c5c;\
+                    width: 18px;\
+                    margin: -2px 0;\
+                    border-radius: 3px;\
+                };");
     connect(slider,SIGNAL(valueChanged(int)),lcdNumber,SLOT(display(int)));
 
     QLabel *scrollBarLabel = new QLabel("&Ползунок:");
@@ -34,12 +61,29 @@ Form::Form(QWidget *parent) : QWidget(parent)
     scrollBarLabel->setBuddy(scrollBar);
     scrollBar->setRange(0,100);
     scrollBar->setStyleSheet(
-   "QScrollBar:horizontal { \
-    border: 2px solid grey; \
-    background: #BCAAA4; \
-    height: 30px; \
-    margin: 0px 0px 0px 0px;}");
-    connect(scrollBar,SIGNAL(valueChanged(int)),lcdNumber,SLOT(display(int)));
+                   "QScrollBar:horizontal { \
+                    border: 2px solid grey; \
+                    background: #00c500; \
+                    height: 30px; \
+                    margin: 0px 20px 0 20px; \
+                } \
+                QScrollBar::handle:horizontal { \
+                    min-width: 20px; \
+                } \
+                QScrollBar::add-line:horizontal { \
+                    border: 2px solid grey; \
+                    background: #00c500; \
+                    width: 20px; \
+                    subcontrol-position: right; \
+                    subcontrol-origin: margin; \
+                } \
+                QScrollBar::sub-line:horizontal { \
+                    border: 2px solid grey; \
+                    background: #00c500; \
+                    width: 20px; \
+                    subcontrol-position: left; \
+                    subcontrol-origin: margin; \
+                }; ");
     connect(scrollBar,SIGNAL(valueChanged(int)),slider,SLOT(setValue(int)));
     connect(slider,SIGNAL(valueChanged(int)),scrollBar,SLOT(setValue(int)));
 
@@ -50,19 +94,71 @@ Form::Form(QWidget *parent) : QWidget(parent)
     dial->setRange(0,100);
     dial->setNotchTarget(10);
     dial->setNotchesVisible(true);
-    connect(dial,SIGNAL(valueChanged(int)),lcdNumber,SLOT(display(int)));
+    dial->setPalette(Qt::darkGreen);
     connect(dial,SIGNAL(valueChanged(int)),slider,SLOT(setValue(int)));
-    connect(dial,SIGNAL(valueChanged(int)),scrollBar,SLOT(setValue(int)));
     connect(slider,SIGNAL(valueChanged(int)),dial,SLOT(setValue(int)));
-    connect(scrollBar,SIGNAL(valueChanged(int)),dial,SLOT(setValue(int)));
 
-    QVBoxLayout *mainLayout = new QVBoxLayout();
-    mainLayout->addWidget(lcdNumber);
-    mainLayout->addWidget(sliderLabel);
-    mainLayout->addWidget(slider);
-    mainLayout->addWidget(scrollBarLabel);
-    mainLayout->addWidget(scrollBar);
-    mainLayout->addWidget(dialLabel);
-    mainLayout->addWidget(dial);
-    setLayout(mainLayout);
+    QVBoxLayout *verticalLayout = new QVBoxLayout;
+    verticalLayout->addWidget(lcdNumber);
+    verticalLayout->addWidget(sliderLabel);
+    verticalLayout->addWidget(slider);
+    verticalLayout->addWidget(scrollBarLabel);
+    verticalLayout->addWidget(scrollBar);
+    verticalLayout->addWidget(dialLabel);
+    verticalLayout->addWidget(dial);
+    firstGroupBox->setLayout(verticalLayout);
+    }
+    //Растягивается через очко
+    {
+    QGroupBox *groupBox = new QGroupBox("Двоичная выводилка");
+    QVBoxLayout *boxLayout = new QVBoxLayout;
+    QLCDNumber *lcdNumber = new QLCDNumber();
+    lcdNumber->setFont(QFont("Times New Roman",15));
+    lcdNumber->setStyleSheet("color: #009a63");
+    lcdNumber->setMode(QLCDNumber::Bin);
+    connect(slider,SIGNAL(valueChanged(int)),lcdNumber,SLOT(display(int)));
+    boxLayout->addWidget(lcdNumber);
+    groupBox->setLayout(boxLayout);
+    mainLayout->addWidget(groupBox);
+    }
+
+    {
+    QGroupBox *groupBox = new QGroupBox("Писанина");
+    mainLayout->addWidget(groupBox);
+
+    QLabel *lineEditLabel = new QLabel("&Редактор строки");
+    lineEditLabel->setFont(baseLabelFont);
+    QLineEdit *lineEdit = new QLineEdit();
+    lineEditLabel->setBuddy(lineEditLabel);
+    lineEdit->setFrame(true);
+    connect(lineEdit,SIGNAL(textChanged(QString)),SLOT(getText(QString)));
+    connect(this, SIGNAL(setNum(int)),slider,SLOT(setValue(int)));
+    connect(slider, SIGNAL(valueChanged(int)),this,SLOT(getNum(int)));
+    connect(this, SIGNAL(setText(QString)),lineEdit,SLOT(setText(QString)));
+
+    QVBoxLayout *verticalLayout = new QVBoxLayout;
+    verticalLayout->addWidget(lineEditLabel);
+    verticalLayout->addWidget(lineEdit);
+    verticalLayout->setSpacing(15);
+    groupBox->setLayout(verticalLayout);
+    }
+}
+
+
+void Form::getText(const QString &text)
+{
+    QVariant converter(text);
+    bool suc;
+    converter.toInt(&suc);
+
+    if(suc){
+        emit setNum(converter.toInt());
+    }
+
+}
+
+void Form::getNum(const int num)
+{
+    QVariant converter(num);
+    emit setText(converter.toString());
 }
--- Сладеры/form.h
+++ Сладеры/form.h
@@ -10,7 +10,11 @@ public:
     explicit Form(QWidget *parent = nullptr);
 
 signals:
-
+    void setNum(int);
+    void setText(QString);
+public slots:
+    void getText(const QString &text);
+    void getNum(const int num);
 };
 
 #endif // FORM_H
